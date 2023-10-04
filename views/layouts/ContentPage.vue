9ba38bddd718aec80e7cb1e872aab6534ccd1c44
lafin716 <lafin716@gmail.com>
2023-10-05 01:56:14 +0900

    feat: 컨텐츠 영역 틀 완성

--- views/layouts/ContentPage.vue
+++ views/layouts/ContentPage.vue
@@ -4,9 +4,24 @@
   <div class="content-wrap">
     <div class="content-header">
       <h2 class="title">HTML의 기초</h2>
-      <span class="created">2023.10.04 12:34:45</span>
+      <div class="info-box">
+        <span class="writer">
+          <Icon name="mdi:account-circle-outline" size="15" />
+          lafin
+        </span>
+        <span class="created">
+          <Icon name="material-symbols:edit-calendar-outline" size="15" />
+          2023.10.04 12:34:45
+        </span>
+        <span class="viewcount">
+          <Icon name="ic:outline-remove-red-eye" size="15" />
+          123
+        </span>
+      </div>
     </div>
-    <slot />
+    <div class="content-body">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -26,8 +41,20 @@
   color: #fff;
 }
 
-.content-wrap .content-header .created {
+.info-box {
+  @apply flex flex-row;
+}
+
+.writer,
+.created,
+.viewcount {
+  @apply mx-2;
   font-size: 15pt;
   color: #fff;
+}
+
+.content-body {
+  @apply px-4 py-2;
+  font-size: 20pt;
 }
 </style>
